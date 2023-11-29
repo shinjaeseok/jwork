dd39f371ec8365ad15bf433bc30dfa551070609e
신재석 <joohong2000@gmail.com>
2023-11-29 09:37:39 +0900

    의존성

--- app/Views/user/home/index.php
+++ app/Views/user/home/index.php
@@ -3,23 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>User Home</title>
-    <!-- 이곳에 스타일 시트 링크 또는 스타일 시트 내용을 추가할 수 있습니다. -->
 </head>
 <body>
-<header>
-    <?php require_once __DIR__ . '/../templates/user_header.php'; ?>
-    <!-- 사용자 헤더를 포함하려면 user_header.php 파일을 추가합니다. -->
-</header>
+
+<?php require_once __DIR__ . '/../templates/user_header.php'; ?>
 
 <main>
     <h1>Welcome to Your User Homepage</h1>
-    <p>Hello, <?= $userName ?>!</p>
-    <!-- 사용자 이름을 출력합니다. -->
+    <p>Hello</p>
 </main>
 
-<footer>
-    <?php require_once __DIR__ . '/../templates/user_footer.php'; ?>
-    <!-- 사용자 푸터를 포함하려면 user_footer.php 파일을 추가합니다. -->
-</footer>
+<?php require_once __DIR__ . '/../templates/user_footer.php'; ?>
+
 </body>
 </html>
